1886560276bbb1625fc127b31b22a2f73e54a9f8
benlerner <benlerner@bitbucket.com>
2015-04-09 22:16:43 -0400

    Fixed sliding
    The Unity 5 upgrade enables the "Apply Root Motion" option for all
    Animators so objects move according to animation and not physics. I
    fixed this to allow her to slide again. Also tweaked the slider script
    so that Zenobia turns properly

--- Unity_Files/Assets/Scripts/PlayerMovement/PlayerSlider.cs
+++ Unity_Files/Assets/Scripts/PlayerMovement/PlayerSlider.cs
@@ -6,7 +6,6 @@ using System.Collections;
 public class PlayerSlider : MonoBehaviour {
 
 	public float jumpSpeed  = 8.0f;
-	public float gravity = 10.0f;
 	private Vector3 moveDirection = Vector3.zero;
 	public Transform tracker;
 	public float rotateSpeed = 30f;
@@ -93,6 +92,7 @@ public class PlayerSlider : MonoBehaviour {
 
 				Vector3 rotateForce = Vector3.left * GetComponent<Rigidbody>().velocity.magnitude * GetComponent<Rigidbody>().mass * direction * turnForce *
 					Mathf.Sin(Vector3.Angle(velocityDirection, facingDirection) * Mathf.Deg2Rad);
+
 				GetComponent<Rigidbody>().AddRelativeForce(rotateForce);
 			}
 
@@ -145,6 +145,6 @@ public class PlayerSlider : MonoBehaviour {
 	//http://answers.unity3d.com/questions/196381/how-do-i-check-if-my-rigidbody-player-is-grounded.html
 	private bool IsGrounded()
 	{
-		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.2f);
 	}
 }
