3c91d6b1a729630f5acb40a3d562c923ede4033c
alexfatemi <videogamesteralex@hotmail.com>
2015-04-21 10:37:54 -0500

    Start of boss level in the files (not in the build)

---
+++ Unity_Files/Assets/Scripts/bossPlatform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bossPlatform : MonoBehaviour {
+	private Vector3 endPos;
+	// Use this for initialization
+	void Start () {
+		endPos = transform.position;
+		endPos.y += 30.0f;
+		iTween.MoveTo (this.gameObject, iTween.Hash("position", endPos, "time", 10.0f, "looptype", "none"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
