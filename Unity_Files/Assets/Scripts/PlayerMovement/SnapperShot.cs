d1dc4af598130ecebd6827503e57211e036e2981
benlerner <benlerner@bitbucket.com>
2015-05-03 00:32:18 -0400

    Put changes back in
    Somehow Jamila's changes got onto my computer without me syncing it,
    which messed up the commit. I reverted the changes and that is fixed
    now.

--- Unity_Files/Assets/Scripts/PlayerMovement/SnapperShot.cs
+++ Unity_Files/Assets/Scripts/PlayerMovement/SnapperShot.cs
@@ -31,9 +31,19 @@ public class SnapperShot : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-		other.gameObject.SendMessage ("snapperShotHit", SendMessageOptions.DontRequireReceiver);
+		if (other.gameObject.tag.Equals("Player"))
+        {
+            Vector3 impactVector = other.transform.TransformPoint(new Vector3(0,1.06f,0)) - transform.position;
+            impactVector.y = 0;
+            impactVector.Normalize();
+            other.GetComponent<Player>().TakeImpactDamage(30f,impactVector, 1000f);
+        } else
+        {
+            other.gameObject.SendMessage ("snapperShotHit", SendMessageOptions.DontRequireReceiver);
+        }
+
 		//maybe create a particle effect?
-	//	Destroy (gameObject);
+		Destroy (gameObject);
     }
     #endregion
 }
