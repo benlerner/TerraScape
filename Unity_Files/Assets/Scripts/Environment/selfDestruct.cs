670d9f98c4f3cc44948b14115c9624de9aa2c05b
unknown <jc3347@71-252-03.drexel.edu>
2015-05-07 14:40:03 -0400

    A bunch of small changes.  2 snapper AIs for different behavior.  One tries to stay at range and the other chases down the player.  Snapper shots, including the player's version are faster and have an distort effect from modified glass.  Changed fadeout on rock remains to always happer rather than being activated by E.

--- Unity_Files/Assets/Scripts/Environment/selfDestruct.cs
+++ Unity_Files/Assets/Scripts/Environment/selfDestruct.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class selfDestruct : MonoBehaviour {
 	public int timeLimit;
 	private int time = 0;
-	private bool setOff = false;
+	private bool setOff = true;
 	// Use this for initialization
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp (KeyCode.E))
-			setOff = true;
+		//if (Input.GetKeyUp (KeyCode.E))
+		//	setOff = true;
 		if (setOff){
 			time++;
 			iTween.FadeUpdate(this.gameObject, 0f, 4f);
